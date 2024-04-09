4cbbbefc22cadd63d6be8dd1735be499c0f3918b
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-04-09 15:02:04 +0900

    시프트 연산자 2024.4.9

---
+++ 비트연산자.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+
+
+
+
+//시프트 연산자
+//int main(void)
+//{
+//	int x = -30;   // 음수도 그냥 2 곱하고 2나누면 됨
+//
+//	printf("%d << 1 = %d\n", x, x << 1);
+//	printf("%d >> 1 = %d\n", x, x >> 1);
+//
+//	return 0;
+//}
+
+
+//비트 연산자
+//int main(void) {
+//	int x = 15; 		// 1111  
+//	int y = 5;   		// 0101
+//
+//	printf("%08X & %08X = %08X\n", x, y, x & y);	// 0101
+//	printf("%08X | %08X = %08X\n", x, y, x | y);	// 1111
+//	printf("%08X ^ %08X = %08X\n", x, y, x ^ y);	// 1010
+//	printf("~ %08X = %08X\n", x, ~x);
+//
+//	return 0;
+//}
