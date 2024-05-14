c968f6fba919e085eb8dc45aecda6258f93eaa0f
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-05-14 12:55:39 +0900

    2024.5.14

---
+++ 함수1.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS_
+#include <stdio.h>
+#include <function_Add.h>
+
+
+
+
+int main(void) {
+
+
+	printf("큰 값 : %d\n", get_max(7, 5));
+	printf("큰 값 : %d\n", get_max(3, 8));
+	printf("큰 값 : %d\n", get_max(-9, 2));
+
+	return 0;
+}
